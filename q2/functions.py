8a427b1395c2d299a50d9e04d1ba690517060ae1
AadilMehdi Sanchawala <aadilmehdis@gmail.com>
2019-09-06 17:05:11 +0530

    Adding the RANSAC function to compute the fundamental matrix

--- q2/functions.py
+++ q2/functions.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import cv2
+import time
 
 
 def correspondingFeatureDetection(img1, img2):
@@ -44,35 +45,80 @@ def correspondingFeatureDetection(img1, img2):
     return kp1_list,kp2_list#Keypoints1, Descriptors1, Keypoints2, Descriptors2, matches
 
 def F_matrix(image_coords_1, image_coords_2):
-    # image_coords_1: N*3 homogeneous coordinates of image pixels    
-    # image_coords_2: N*3 homogeneous coordinates of image pixels
+    '''
+    image_coords_1  : N*3 homogeneous coordinates of image pixels    
+    image_coords_2  : N*3 homogeneous coordinates of image pixels
+    return          : Fundamental Matrix of dimension 3*3
+    '''
     
     A = np.zeros((len(image_coords_1),9))
-    # A.shape
-    # Getting the A matrix of size N*9
+
+    # Constructing A matrix of size N*9
     for i in range(len(image_coords_1)):
         A[i,:] = np.kron(image_coords_1[i,:], image_coords_2[i,:])
         
     u, s, vh = np.linalg.svd(A, full_matrices=True)
-    print(vh[8,:])
     F = np.reshape(vh[8,:], (3,3))
-    print(F)
     uf, sf, vhf = np.linalg.svd(F, full_matrices=True)
     
     F = uf @ np.diag(np.array([sf[0], sf[1], 0])) @ vhf
 
     return F
 
+def F_RANSAC(image_points_1, image_points_2, threshold, n_iters):
+    '''
+        image_points_1  : N*3 matrix of a normalized 2D homogeneous of image 1 
+        image_points_2  : N*3 matrix of a normalized 2D homogeneous of image 2
+        threshold       : Inlier threshold
+        n_iters         : Number of Iterations 
+        return          : Fundamental Matrix of dimension 3*3
+    '''
+
+    n = 8 
+    F = np.zeros((3,3))
+    max_inliers = -9999999
+
+    for i in range(n_iters):
+
+        # Randomly sample 8 matching points from image 1 and image 2
+        indices = np.random.choice(image_points_1.shape[0], n, replace=False)  
+        matched_points_1 = image_points_1[indices] 
+        matched_points_2 = image_points_2[indices]
+
+        # Get the F matrix for the current iteration
+        F_current = F_matrix(matched_points_1, matched_points_2)
+
+        # Counting the number of inliers 
+        number_current_inliers = 0
+        for i in range(len(image_points_1)):
+            error = np.abs(image_points_2[i,:] @ F_current @ image_points_1[i,:].T)
+            if error < threshold:
+                number_current_inliers += 1
+
+        # Updating F matrix
+        if number_current_inliers > max_inliers:
+            max_inliers = number_current_inliers
+            F = F_current
+    return F
+
 
 def NormalizationMat(image_coords):
-    # image_coords Nx3
+    '''
+        image_coords : N*3 matrix of image coordinates
+        return       : 3*3 Scale Transformation matrix 
+    '''
+
     mu = np.mean(image_coords,axis = 0)
     d = 0
     for i in range(len(image_coords)):
         d = d + np.sqrt((image_coords[i,0] - mu[0])**2 + (image_coords[i,1] - mu[1])**2)
 
-    d = d / i
-    T = np.mat([[1.44/d, 0, -1.44*mu[0] / d], [0, 1.44 / d,-1.44*mu[1] / d],[0, 0, 1]])
+    d = d/i
+    T = np.mat([
+        [1.44/d, 0, -1.44 * mu[0]/d], 
+        [0, 1.44/d, -1.44 * mu[1]/d],
+        [0,      0,               1]
+        ])
     
     return T
     
@@ -80,18 +126,20 @@ def NormalizationMat(image_coords):
 
 if __name__ == "__main__":
 
-    img1 = cv2.imread(r'../../mr19-assignment2-data/images/000000.png')
-    img2 = cv2.imread(r'../../mr19-assignment2-data/images/000001.png')
-    # plt.imshow(img1)
-    # plt.show()
+    img1 = cv2.imread('../mr19-assignment2-data/images/000000.png')
+    img2 = cv2.imread('../mr19-assignment2-data/images/000001.png')
+
     kp1, kp2 = correspondingFeatureDetection(img1, img2)
     T1 = NormalizationMat(kp1[0:9,:])
     T2 = NormalizationMat(kp2[0:9,:])
 
-    points1 = T1@kp1[0:9,:].T
-    points2 = T2@kp2[0:9,:].T
-    # print(points1.T)
-    F = F_matrix(points1.T,points2.T)
-    # print(F)
+    points1 = T1 @ kp1.T
+    points2 = T2 @ kp2.T
+
+
+    F = F_RANSAC(points1.T, points2.T, 0.005, 500)
+
+    F = T2.T @ F @ T1
+
     for i in range(8):
         print(kp2[i,:]@T2.T@F@T1@kp1[i,:].T)
