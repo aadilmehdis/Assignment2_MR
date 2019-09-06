ae92e57b46b33781f8691afdae1ac4b5a34c3f88
RahulSajnani <rahulsajnani2010@hotmail.com>
2019-09-06 11:24:21 +0530

    recovered keypoints in functions.py

--- q2/functions.py
+++ q2/functions.py
@@ -17,19 +17,23 @@ def correspondingFeatureDetection(img1, img2):
 
     # Sort them in the order of their distance.
     matches = sorted(matches, key = lambda x:x.distance)
-    for m in matchs:
+    kp1_list = []
+    kp2_list = []
+    for m in matches:
 
         img1Idx = m.queryIdx
         img2Idx = m.trainIdx
 
-        (img1x, img1y) = Keypoints1[img1Idx]
-        (img2x, img2y) = Keypoints2[img2Idx]
-        
+        (img1x, img1y) = Keypoints1[img1Idx].pt
+        (img2x, img2y) = Keypoints2[img2Idx].pt
+
+        kp1_list.append((img1x,img1y))
+        kp2_list.append((img2x,img2y))
         
     # Draw first 10 matches.
     img3 = cv2.drawMatches(img1,Keypoints1,img2,Keypoints2,matches[:30],None, flags=2)
 
-    # plt.imshow(img3),plt.show()
+    plt.imshow(img3),plt.show()
     return Keypoints1, Descriptors1, Keypoints2, Descriptors2, matches
 
 
@@ -38,7 +42,7 @@ def correspondingFeatureDetection(img1, img2):
 if __name__ == "__main__":
 
     img1 = cv2.imread(r'../../mr19-assignment2-data/images/000000.png')
-    img2 = cv2.imread(r'../../mr19-assignment2-data/images/000000.png')
+    img2 = cv2.imread(r'../../mr19-assignment2-data/images/000001.png')
     # plt.imshow(img1)
     # plt.show()
     correspondingFeatureDetection(img1, img2)
